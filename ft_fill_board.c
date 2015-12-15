e9424e2fad78b7393667066394bd9cd76aa6a45b
qdegraev <qdegraev@student.42.fr>
2015-12-15 15:55:03 +0100

    correction ft_fill_board

--- ft_fill_board.c
+++ ft_fill_board.c
@@ -6,7 +6,7 @@
 /*   By: afillion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 15:09:32 by afillion          #+#    #+#             */
-/*   Updated: 2015/12/14 19:34:52 by afillion         ###   ########.fr       */
+/*   Updated: 2015/12/15 15:51:14 by qdegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,13 @@ void	ft_fill_board(char **map, char **tab, int x, int y)
 	ft_putendl("fill board");//TEST
 	while (tab[i])
 	{
-		if (tab[i][j] == map[x + i][y + j])
-			j++;
-		if (tab[i][j] != map[x + i][y + j] && j < ft_strlen(tab[i]))
+		while (tab[i][j])
 		{
 			map[x + i][y + j] = tab[i][j];
 			j++;
 		}
-		if (tab[i][j] == '\0')
-		{
-			i++;
-			j = 0;
-		}
+		i++;
+		j = 0;
 	}
 	ft_putendl("fill board fin");//TEST
 	return ;
@@ -57,6 +52,7 @@ void	ft_niketamerelapute(char **map, t_list *lst)
 				ft_putendl("check board = 1");//TEST
 				ft_fill_board(map, lst->tab, i, j);
 				lst = lst->next;
+				j = 0;
 			}
 			else
 				j++;
@@ -73,28 +69,20 @@ int		check_board(char **map, char **tab, int x, int y)
 	i = 0;
 	j = 0;
 	ft_putendl("check board entree");//TEST
-	while (tab[i] && map[x + i])
+	while (tab[i])
 	{
 		j = 0;
-		while (tab[i][j] && map[x + i][y + j])
+		while (tab[i][j])
 		{
-			ft_putnbr(j);
-			ft_putendl(tab[i]);
-			ft_putendl(map[x + i]);
-			ft_putchar(map[x + i][y + j]);
-			ft_putchar('\n');
-			ft_putchar(tab[i][j]);
-			ft_putchar('\n');
-			if (map[x + i][y + j] == '.' && (tab[i][j] == '#' || tab[i][j] == '.'))
-			{
-				ft_putendl("premier if");
-				j++;
-			}
-			/*if (map[x + i][y + j] == '#' && tab[i][j] == '.')
+/*			if (map[x + i][y + j] == '#' && tab[i][j] == '.')
 			{
 				ft_putendl("deuxieme if");
 				j++;
-			}*/
+			}
+*/			if (map[x + i][y + j] == '.' && tab[i][j] == '#')
+				j++;
+			else if (map[x + i][y + j] == '.' && tab[i][j] == '.')
+				j++;
 			else
 				return (0);
 		}
